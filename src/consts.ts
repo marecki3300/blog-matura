d2af4dfe1c868671d73aa0019d12ec00830d9660
marecki3300 <oleszm@hotmail.com>
2026-04-23 09:41:20 +0200

    nazwa bloga i usuwanie przykładowych artykułów

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Angielski do matury – Kasia English Teacher';
+export const SITE_DESCRIPTION = 'Zadania gramatyczne, ćwiczenia i porady na maturę z angielskiego 2026';
